9ab7d15cdc50099a1a751bb2d26f08335214828d
Guilherme P Schneidt <guilhermepsch@gmail.com>
2024-01-13 16:29:30 -0300

    feat: refatorado componente home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './views/Home';
 import BookController from './controllers/BookController';
 import { BookRepository } from './repositories/BookRepository';
 
